f3165c7b7251fb0e2670744b877cac807d71d343
Khaled Hamada <hh371551@gmail.com>
2026-07-23 20:08:31 +0300

    feat: implement Department model

--- EmployeeManagementSystem/Models/Department.cs
+++ EmployeeManagementSystem/Models/Department.cs
@@ -8,6 +8,13 @@ namespace EmployeeManagementSystem.Models
 {
     public class Department
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public string GetInfo()
+        {
+            return $"Department: {Name}";
+        }
 
     }
 }
