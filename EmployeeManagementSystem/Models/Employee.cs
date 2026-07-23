78f2cf1f4153cbd0fd01c18225097fe21fec353f
Khaled Hamada <hh371551@gmail.com>
2026-07-23 19:48:53 +0300

    feat: implement Employee model

--- EmployeeManagementSystem/Models/Employee.cs
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -8,6 +8,23 @@ namespace EmployeeManagementSystem.Models
 {
     public class Employee
     {
+        public int Id {get; set;}
+        public string Name {get; set;}
+        public DateTime HireDate {get; set;}
+        public int DepartmentId {get; set;}
+        public decimal Salary {get; set;}
 
+        public Employee()
+        {
+            Name = string.Empty;
+            HireDate = DateTime.Now;
+            Salary = 0.0m;
+        }
+        public virtual string GetInfo()
+        {
+            return $"Name : {Name}" +
+            $"\nHire Date : {HireDate:d}" +
+            $"\nSalary : {Salary:C}" ;
+        }
     }
 }
